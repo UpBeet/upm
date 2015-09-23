f724f0deced88e5685f0cab017467d3e077317a0
Will <whp3652@rit.edu>
2015-09-23 02:38:47 -0400

    Complete initial draft of design doc

--- config.rb
+++ config.rb
@@ -48,10 +48,13 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+set :fonts_dir, 'fonts'
+
+activate :deploy do |deploy|
+  deploy.method = :git
+end
 
 # Build-specific configuration
 configure :build do
